4e9c7bb7984ec567a950f3c96c75309fbf62bc04
Anna Sidorova <anna_sidorova@senla.eu>
2026-07-12 15:17:50 +0300

    тесты еще для DailyVitals

--- src/test/java/tests/DailyVitalsTest.java
+++ src/test/java/tests/DailyVitalsTest.java
@@ -1,5 +1,9 @@
 package tests;
 
+import dto.DailyVitals;
+import dto.DailyVitalsFactory;
+import dto.FeedBackFactory;
+import dto.Feedback;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -26,17 +30,54 @@ public class DailyVitalsTest extends BaseTest{
         softAssert.assertAll();
     }
 
+
+    DailyVitals dailyVitals = DailyVitalsFactory.getDailyVitals();
+
     @Test(testName = "Добавление ежедневных показателей"
             ,  description = "Поля Weight, Resting HR, Sleep Hours и др. заполняются и сохраняются ")
-    public void addVital() {
+    public void checkAddVital() {
+        SoftAssert softAssert = new SoftAssert();
+        DailyVitals dailyVitals = DailyVitalsFactory.getDailyVitals();
+        String expectedHealthNotes = dailyVitals.getHealthNotes();
+        loginPage.openPage();
+        loginPage.enterCreds(email, password);
+        loginPage.pushLoginButton();
+        dailyVitalsPage.openPage()
+                .isPageOpened()
+                        .clickAddVitalButton()
+                                .fillVital(dailyVitals)
+                                        .clickSaveVitalButton();
+        isTextDisplayed(expectedHealthNotes);
+        softAssert.assertAll();
+    }
+
+    @Test(testName = "Редактирование записи показателей"
+            ,  description = "После подтверждения запись отредактирована")
+    public void checkEdditVital() {
+        SoftAssert softAssert = new SoftAssert();
+        DailyVitals dailyVitals = DailyVitalsFactory.getDailyVitals();
+        String expectedHealthNotes = dailyVitals.getHealthNotes();
+        loginPage.openPage();
+        loginPage.enterCreds(email, password);
+        loginPage.pushLoginButton();
+        dailyVitalsPage.openeditedPage()
+                .editVital(dailyVitals)
+                .clickSaveVitalButton();
+        isTextDisplayed(expectedHealthNotes);
+        softAssert.assertAll();
+    }
+
+    @Test(testName = "График изменения веса за период"
+            ,  description = "График отображает динамику веса по дням, значения корректны")
+    public void checkWeightChange() {
         SoftAssert softAssert = new SoftAssert();
         loginPage.openPage();
         loginPage.enterCreds(email, password);
         loginPage.pushLoginButton();
         dailyVitalsPage.openPage()
                 .isPageOpened()
-                        .clickAddVitalButton();
-        //дописать
+                        .findWeightIcon();
+
         softAssert.assertAll();
     }
 }
