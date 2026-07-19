998ea0fd47285e06ede06a67500983c78ca56e68
AnnaAtsu <AnnaAtsu@users.noreply.github.com>
2026-07-19 14:50:42 +0000

    deploy: 9b0801408a4f14010013fbbe0eddd7d6ed4032f9

--- src/test/java/constants/ConstantElements.java
+++
@@ -1,9 +0,0 @@
-package constants;
-
-import utils.ConfigReader;
-
-public class ConstantElements {
-
-    public final static String email = ConfigReader.getEmail();
-    public final static  String password = ConfigReader.getPassword();
-}
